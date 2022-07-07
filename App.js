a068cb6431343cd4414ac4b64e48143228548caf
Noah <noahcuroe@gmail.com>
2022-07-06 21:31:22 -0500

    stack demo

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+function AScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>A</Text>
+            <Button title="Go to B" onPress={() => navigation.navigate('B')}></Button>
+        </View>
+    );
+}
+
+function BScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>B</Text>
+            <Button title="Go to C" onPress={() => navigation.navigate('C')}></Button>
+            <Button title="Go back" onPress={() => navigation.goBack()}></Button>
+        </View>
+    );
+}
+
+function CScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>C</Text>
+            <Button title="Go back" onPress={() => navigation.goBack()}></Button>
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="A">
+            <Stack.Screen name="A" component={AScreen} />
+            <Stack.Screen name="B" component={BScreen} />
+            <Stack.Screen name="C" component={CScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
